637a819b402f2ae33a59d48127bb1b12e96138c0
王碧瑶 <2487036948@qq.com>
2019-08-03 17:54:24 +0800

    实现日期类

---
+++ test_2019_8_3_1/test_2019_8_3_1/Date.cpp
@@ -0,0 +1,146 @@
+#include "Date.h"
+bool Date::IsLeapYear(int year)//判断闰年
+{
+	if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+		return true;
+	else
+		return false;
+}
+int Date::GetMonthDay(int year, int month)//根据已知的年月直到该月的天数
+{
+	int Month_array[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int day = Month_array[month];
+	if (month == 2 && IsLeapYear(year))//这时2月有29天
+	{
+		day += 1;
+	}
+	return day;
+}
+bool Date::IsValid(int year, int month, int day)//判断日期是否合法
+{
+	if (year >= 0 && month >0 && month<13 && day>0 && day <= GetMonthDay(year, month))
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	return true;
+}
+
+Date::Date(int year , int month , int day )//构造函数
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+		if (!IsValid(_year, _month, _day))
+		{
+			assert(false);
+			cout << "非法日期" << endl;
+		}
+	}
+Date::Date(const Date& d)//拷贝构造函数
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+Date& Date:: operator= (const Date& d)//赋值
+{
+	if (*this != d)//在保证不是自己给自己赋值
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+	return *this;
+}
+Date& Date:: operator++()//前置
+{
+	_day += 1;
+	return *this;
+}
+Date Date:: operator++(int)//后置
+{
+	Date tmp(*this);
+	_day += 1;
+	return tmp;
+}
+Date& Date:: operator--()//前置
+{
+	_day -= 1;
+	return *this;
+}
+Date Date:: operator--(int)//后置
+{
+	Date tmp(*this);
+	_day -= 1;
+	return tmp;
+}
+bool Date:: operator==(const Date& d)//判断本日期是否与d相等
+{
+	return (_year == d._year) && (_month == d._month) && (_day == d._day);
+}
+bool Date:: operator!=(const Date& d)
+{
+	return !(*this == d);
+}
+bool Date::operator<(const Date& d)
+{
+	if (_year < d._year)
+		return true;
+	else if (_year == d._year&&_month < d._month)
+		return true;
+	else if (_year == d._year&&_month == d._month&&_day < d._day)
+		return true;
+	else
+		return false;
+}
+bool Date:: operator>(const Date& d)
+{
+	if (_year > d._year)
+		return true;
+	else if (_year == d._year&&_month > d._month)
+		return true;
+	else if (_year == d._year&&_month == d._month&&_day > d._day)
+		return true;
+	else
+		return false;
+}
+bool Date:: operator<=(const Date& d)
+{
+	return *this < d || *this == d;
+}
+bool Date::operator>=(const Date& d)
+{
+	return *this>d || *this == d;
+}
+void Date:: show()
+{
+	cout << _year << " " << _month << " " << _day << endl;
+}
+int main()
+{
+	Date d1(2019, 8, 3);
+	d1.show();
+	Date d2(2019, 8, 4);
+	d2.show();
+	cout << (d1 == d2) << endl;
+	cout << (d1 != d2) << endl;
+	cout << (d1 < d2) << endl;
+	cout << (d1 > d2) << endl;
+	cout << (d1 >= d2) << endl;
+	cout << (d1 <= d2) << endl;
+	d1 = d2;
+	d1.show();
+	d2++;
+	d2.show();
+	++d2;
+	d2.show();
+	d1--;
+	d1.show();
+	--d1;
+	d1.show();
+	return 0;
+}
+	
+
---
+++ test_2019_8_3_1/test_2019_8_3_1/Date.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <assert.h>
+using namespace std;
+class Date
+{
+public:
+	bool IsLeapYear(int year);//判断闰年
+	int GetMonthDay(int year, int month);//根据已知的年月直到该月的天数
+	bool IsValid(int year, int month, int day);//判断日期是否合法
+	Date(int year , int month , int day );//构造函数
+	Date(const Date& d);//拷贝构造函数
+	//赋值运算符重载
+	Date& operator= (const Date& d);//赋值
+	Date& operator++();//前置
+	Date operator++(int);//后置
+	Date& operator--();//前置
+	Date operator--(int);//后置
+	bool operator==(const Date& d);//判断本日期是否与d相等
+	bool operator!=(const Date& d);
+	bool operator<(const Date& d);
+	bool operator>(const Date& d);
+	bool operator<=(const Date& d);
+	bool operator>=(const Date& d);
+	void show();
+private:
+	int _year;
+	int _month;
+	int _day;
+};
