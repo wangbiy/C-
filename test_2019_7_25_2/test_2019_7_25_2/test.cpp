0602ba05f143bb6f35aa3cfe2d6cd8d80130508c
王碧瑶 <2487036948@qq.com>
2019-08-03 15:22:49 +0800

    类和对象2

--- test_2019_7_25_2/test_2019_7_25_2/test.cpp
+++ test_2019_7_25_2/test_2019_7_25_2/test.cpp
@@ -211,6 +211,7 @@ int main()
 	return 0;
 }
 #endif
+#if 0
 class String
 {
 public:
@@ -237,3 +238,35 @@ int main()
 	String s2(s1);//拷贝构造函数，如果没有显式实现，编译器会自动生成
 	return 0;
 }
+#endif
+class Time
+{
+public:
+	Time()
+	{
+		cout << "Time():" << endl;
+		_hour = 0;
+		_minute = 0;
+		_second = 0;
+	}
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
+class Date
+{
+private:
+	//内置类型
+	int _year;
+	int _month;
+	int _day;
+	//自定义类型
+	Time _t;
+};
+int main()
+{
+	Date d;
+	return 0;
+}
+
