fb9a8e3973db71787bce3d4a6e4ce53b2197730f
王碧瑶 <2487036948@qq.com>
2019-08-17 18:13:30 +0800

    暑假作业

--- test_2019_8_12/test_2019_8_12/test.cpp
+++ test_2019_8_12/test_2019_8_12/test.cpp
@@ -694,5 +694,48 @@ int main()
 	return 0;
 }
 #endif
+#if 0
+#define sum(a,b,c) a+b+c
+int main()
+{
+	int i = 3;
+	int j = 2;
+	printf("%d\n", i*sum(i, (i + j), j));
+	system("pause");
+	return 0;
 
+}
+#endif
+#if 0
+int f(int n)
+{
+	static int i = 0;
+	if (n >= 5)
+		return n;
+	n = n + i;
+	i++;
+	return f(n);
+}
+int main()
+{
+	printf("%d\n", f(1));
+	system("pause");
+	return 0;
+}
+#endif
+int func()
+{
+	int i, j, k = 0;
+	for (i = 0, j = -1; j = 0; i++, j++)
+	{
+		k++;
+	}
+	return k;
+}
+int main()
+{
+	printf("%d\n", func());
+	system("pause");
+	return 0;
+}
 
