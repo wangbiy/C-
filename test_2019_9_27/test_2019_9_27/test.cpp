08d22d71c59b333ff1f15e23e40931857c479a5d
王碧瑶 <2487036948@qq.com>
2019-09-28 22:47:23 +0800

    多态

--- test_2019_9_27/test_2019_9_27/test.cpp
+++ test_2019_9_27/test_2019_9_27/test.cpp
@@ -205,6 +205,7 @@ int main()
 }
 #endif
 #if 0
+#include <string>
 class Base
 {
 public:
@@ -231,36 +232,48 @@ public:
 	}
 	virtual void Func3()
 	{
-		cout << "DErived::Func3()" << endl;
+		cout << "Derived::Func3()" << endl;
 	}
 	virtual void Func4()
 	{
-		cout << "DErived::Func4()" << endl;
+		cout << "Derived::Func4()" << endl;
 	}
 	virtual void Func5()
 	{
-		cout << "DErived::Func5()" << endl;
+		cout << "Derived::Func5()" << endl;
 	}
 	int _d;
 };
-typedef void(*PVFT)();
-void printVFT(Base& b)
-{
-	PVFT* pVFT = (PVFT*)(*(int*)&b);
-	while (*pVFT)
-	{
-		(*pVFT)();
-		++pVFT;
-	}
-	cout << endl;
-}
+//typedef void(*PVFT)();
+//void printVFT(Base& b,const string& str)
+//{
+//	cout << str << endl;
+//	PVFT* pVFT = (PVFT*)(*(int*)&b);
+//	while (*pVFT)
+//	{
+//		(*pVFT)();
+//		++pVFT;
+//	}
+//	cout << endl;
+//}
 void TestVirtual(Base& b)
 {
 	b.Func1();
 	b.Func2();
 	b.Func3();
 }
-void Test1()
+//void Test()
+//{
+//	Base b;
+//	b._b = 1;
+//	Derived d;
+//	d._b = 1;
+//	d._d = 2;
+//	printVFT(b,"Base VFT");
+//	printVFT(d, "Base VFT");
+//
+//}
+int main()
 {
 	Base b;
 	b._b = 1;
@@ -268,27 +281,200 @@ void Test1()
 	d._b = 1;
 	d._d = 2;
 	TestVirtual(b);
-	cout << "***************" << endl;
 	TestVirtual(d);
-}
-void Test2()
-{
-	Base b;
-	b._b = 1;
-	Derived d;
-	d._b = 1;
-	d._d = 2;
-	printVFT(b);
-	cout << "*****************" << endl;
-	printVFT(d);
-
-}
-int main()
-{
-	//Test1();
-	Test2();
+	Base* pb = (Base*)&d;//Derived*
+	pb->Func1();
 	return 0;
 }
 #endif
+#if 0
+class B1
+{
+public:
+	virtual void Func1()
+	{
+		cout << "B1::Func1()" << endl;
+	}
+	virtual void Func2()
+	{
+		cout << "B1::Func2()" << endl;
+	}
+	int _b1;
+};
+class B2
+{
+public:
+	virtual void Func3()
+	{
+		cout << "B2::Func3()" << endl;
+	}
+	virtual void Func4()
+	{
+		cout << "B2::Func4()" << endl;
+	}
+	int _b2;
+};
+class D :public B1, public B2
+{
+public:
+	virtual void Func1()//对B1中的Func1进行重写
+	{
+		cout << "D::Func1()" << endl;
+	}
+	virtual void Func4()//对B2中的Func4进行重写
+	{
+		cout << "D::Func4()" << endl;
+	}
+	virtual void Func5()
+	{
+		cout << "D::Func5()" << endl;
+	}
+	int _d;
+};
+typedef void(*PVFT)();
+#include <string>
+void PrintVFT1(B1& b, const string& str)
+{
+	cout << str << endl;
+	PVFT* pVFT = (PVFT*)(*(int*)(&b));
+	while (*pVFT)//虚表中最后一行是00 00 00 00
+	{
+		(*pVFT)();
+		++pVFT;
+	}
+	cout << endl;
+}
+void PrintVFT2(B2& b, const string& str)
+{
+	cout << str << endl;
+	PVFT* pVFT = (PVFT*)(*(int*)(&b));
+	while (*pVFT)//虚表中最后一行是00 00 00 00
+	{
+		(*pVFT)();
+		++pVFT;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	cout << sizeof(D) << endl;
+	D d;
+	d._b1 = 1;
+	d._b2 = 2;
+	d._d = 3;
+	PrintVFT1(d, "override B1");
+	PrintVFT2(d, "override B2");
+	return 0;
+}
+#endif
+class B
+{
+public:
+	virtual void Func1()
+	{
+		cout << "B::Func1()" << endl;
+	}
+	virtual void Func2()
+	{
+		cout << "B::Func2()" << endl;
+	}
+	int _b;
+};
+class C1 :virtual public B
+{
+public:
+	virtual void Func1()//对B中的Func进行重写
+	{
+		cout << "C1::Func()" << endl;
+	}
+	virtual void Func3()
+	{
+		cout << "C1::Func3()" << endl;
+	}
+	int _c1;
+};
+class C2 :virtual public B
+{
+public:
+	virtual void Func2()//对B中的Func2进行重写
+	{
+		cout << "C2::Func2()" << endl;
+	}
+	virtual void Func4()
+	{
+		cout << "C2::Func4()" << endl;
+	}
+	int _c2;
+};
+class D :public C1, public C2
+{
+public:
+	virtual void Func1()//对C1中的Func1进行重写
+	{
+		cout << "D::Func1()" << endl;
+	}
+	virtual void Func3()//对C1中的Func3进行重写
+	{
+		cout << "D::Func2()" << endl;
+	}
+	virtual void Func4()//对C2中的Func4进行重写
+	{
+		cout << "D::Func4()" << endl;
+	}
+	virtual void Func5()//新增加的
+	{
+		cout << "D::Func5()" << endl;
+	}
+	int _d;
+};
+#include <string>
+typedef void(*PVFT)();
+void PrintVFT1(C1& b, const string& str)
+{
+	cout << str << endl;
+	PVFT* pVFT = (PVFT*)(*(int*)(&b));
+	while (*pVFT)
+	{
+		(*pVFT)();
+		++pVFT;
+	}
+	cout << endl;
+}
+void PrintVFT2(C2& b, const string& str)
+{
+	cout << str << endl;
+	PVFT* pVFT = (PVFT*)(*(int*)(&b));
+	while (*pVFT)
+	{
+		(*pVFT)();
+		++pVFT;
+	}
+	cout << endl;
+}
+void PrintVFT3(B& b, const string& str)
+{
+	cout << str << endl;
+	PVFT* pVFT = (PVFT*)(*(int*)(&b));
+	while (*pVFT)
+	{
+		(*pVFT)();
+		++pVFT;
+	}
+	cout << endl;
+}
+int main()
+{
+	cout << sizeof(D) << endl;
+	D d;
+	d._b = 1;
+	d._c1 = 2;
+	d._c2 = 3;
+	d._d = 4;
+	PrintVFT1(d, "override C1");
+	PrintVFT2(d, "override C2");
+	PrintVFT3(d, "override B");
+	return 0;
+}
 
 
